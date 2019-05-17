839a5ef4a734cb38ee6886c9a6bd54bce6a26cb0
Soo Hyeok Lee <39535216+soohyeok@users.noreply.github.com>
2019-05-16 21:17:49 -0400

    Create script.js

---
+++ Gameboard/script.js
@@ -0,0 +1,13 @@
+var main = function() {
+	$('.more-btn').on('click', (event)=>{
+    $(event.currentTarget).siblings('.more-menu').toggle();
+  });
+  $('.share').on('click', (event)=>{
+    $(event.currentTarget).siblings('.share-menu').toggle();
+  });
+  $('.notification').on('click', (event)=>{
+    $(event.currentTarget).toggleClass('active');
+  });
+};
+
+$(document).ready(main);
