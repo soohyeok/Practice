57863094cf8fae04134fcee58f339063b8f423e5
Soo Hyeok Lee <39535216+soohyeok@users.noreply.github.com>
2019-09-13 17:53:03 -0400

    Create project_init.js

---
+++ WeatherApplication/project_init.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var searchButton = document.querySelector('button');
+var searchCity = document.querySelector('#city');
+
+var loadingText = document.querySelector('#load');
+var weatherBox = document.querySelector('#weather');
+
+var weatherCity = weatherBox.firstElementChild; //firstElementChild
+var weatherDesc = document.querySelector('#weatherDescription'); //#weatherDescription
+var weatherTemp = weatherBox.lastElementChild; //lastElementChild
+
+
+
+
